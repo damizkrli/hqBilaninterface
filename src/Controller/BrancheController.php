c37e193051910b5c56bf512712799cbd2ae3b288
damizkrli <karli.damien@gmail.com>
2022-07-13 07:40:30 +0200

    Affichage de BrancheController

--- src/Controller/BrancheController.php
+++ src/Controller/BrancheController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Branche;
+use App\Repository\BrancheRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BrancheController extends AbstractController
 {
-    #[Route('/branche', name: 'app_branche')]
+    private BrancheRepository $brancheRepository;
+
+    public function __construct(BrancheRepository $brancheRepository)
+    {
+        $this->brancheRepository = $brancheRepository;
+    }
+
+    #[Route('/', name: 'app_branche')]
     public function index(): Response
     {
+        $branches = $this->brancheRepository->findAll();
+
         return $this->render('branche/index.html.twig', [
-            'controller_name' => 'BrancheController',
+            'branches' => $branches,
         ]);
     }
+
+
 }
